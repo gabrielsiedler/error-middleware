e241f36962cd508408d1448bd0ac387924d67fbb
Gabriel Siedler <gabrielsiedler@gmail.com>
2018-06-24 10:40:01 -0300

    Adding 'proto' back to custom error

--- src/errors/custom-error.ts
+++ src/errors/custom-error.ts
@@ -5,12 +5,17 @@ export interface Output {
 }
 
 export abstract class CustomError extends Error {
+  // tslint:disable-next-line:variable-name
+  public __proto__: Error;
   public message: string;
   public code: number;
   public type: string;
 
   constructor(message = '', code = 400, type = 'CustomError') {
+    const trueProto = new.target.prototype;
     super();
+
+    this.__proto__ = trueProto;
 
     this.message = message;
     this.code = code;

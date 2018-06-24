0d7554012a6f0439d1b7837fd9d9618a9a226fc7
Gabriel Siedler <gabrielsiedler@gmail.com>
2018-06-24 12:33:30 -0300

    Adding internal error tests

---
+++ src/errors/internal-error/internal-error.spec.ts
@@ -0,0 +1,43 @@
+import { CustomError } from '../custom-error';
+import { InternalError } from './internal-error';
+
+const DEFAULT_INTERNAL_MESSAGE = 'An error occurred. Please try again later.';
+
+describe('InternalError', () => {
+  describe('when object is created with default args', () => {
+    it('is instance of CustomError', () => {
+      const error = new InternalError();
+
+      expect(error instanceof CustomError).toBeTruthy();
+    });
+
+    it('is instance of InternalError', () => {
+      const error = new InternalError();
+
+      expect(error instanceof InternalError).toBeTruthy();
+    });
+
+    describe('when output is called', () => {
+      it('returns default error and code', () => {
+        const error = new InternalError();
+
+        const output = error.output();
+
+        expect(output.code).toEqual(500);
+        expect(output.error).toEqual(DEFAULT_INTERNAL_MESSAGE);
+      });
+    });
+  });
+
+  describe('when object is created with custom args', () => {
+    describe('when output is called', () => {
+      it('does not return custom error', () => {
+        const error = new InternalError('Custom message.');
+
+        const output = error.output();
+
+        expect(output.error).toEqual(DEFAULT_INTERNAL_MESSAGE);
+      });
+    });
+  });
+});

669dad8ef7ef84625c67fcfbdcf126bd5cb635d8
Gabriel Siedler <gabrielsiedler@Gabriels-MacBook-Pro.local>
2020-05-21 15:27:08 -0300

    adding custom error and tests

---
+++ src/errors/custom-error/custom-error.spec.ts
@@ -0,0 +1,52 @@
+import { BaseError } from '../base-error/base-error'
+import { CustomError } from './custom-error'
+
+describe('CustomError', () => {
+  describe('when object is created with default args', () => {
+    it('is instance of BaseError', () => {
+      const error = new CustomError()
+
+      expect(error instanceof BaseError).toBeTruthy()
+    })
+
+    it('is instance of CustomError', () => {
+      const error = new CustomError()
+
+      expect(error instanceof CustomError).toBeTruthy()
+    })
+
+    describe('when output is called', () => {
+      it('returns default error, code and type', () => {
+        const error = new CustomError()
+
+        const output = error.output()
+
+        expect(output.code).toEqual(400)
+        expect(output.error).toEqual('Error.')
+        expect(output.type).toEqual('CustomError')
+      })
+    })
+  })
+
+  describe('when object is created with custom args', () => {
+    describe('when output is called', () => {
+      it('returns custom error', () => {
+        const error = new CustomError('Custom message.', 406, 'NotJava')
+
+        const output = error.output()
+
+        expect(output.code).toEqual(406)
+        expect(output.error).toEqual('Custom message.')
+        expect(output.type).toEqual('NotJava')
+      })
+
+      it('returns custom error type', () => {
+        const error = new CustomError({ myError: { deep: true, more: [null, 'some'] } })
+
+        const output = error.output()
+
+        expect(output.error).toEqual({ myError: { deep: true, more: [null, 'some'] } })
+      })
+    })
+  })
+})
--- src/errors/custom-error/custom-error.ts
+++ src/errors/custom-error/custom-error.ts
@@ -1,7 +1,7 @@
 import { BaseError } from '../base-error/base-error'
 
 export class CustomError extends BaseError {
-  constructor(message: any = 'Error.', code: Number = 400, type: string = 'Error') {
+  constructor(message: any = 'Error.', code: Number = 400, type: string = 'CustomError') {
     super(message, code, type)
   }
 }
